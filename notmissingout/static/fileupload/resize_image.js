584ee41e00f41ec37187c6685ca3a412dec7b31d
Richard Boulton <richard@tartarus.org>
2017-01-22 16:35:07 +0000

    Add summernote overrides to enable client side image resizing

---
+++ notmissingout/static/fileupload/resize_image.js
@@ -0,0 +1,15 @@
+// Bit of JS to enable client side image resizing
+
+/*
+$('#fileupload').fileupload({
+    dataType: 'json',
+    // Enable image resizing, except for Android and Opera,
+    // which actually support image resizing, but fail to
+    // send Blob objects via XHR requests:
+    disableImageResize: /Android(?!.*Chrome)|Opera/
+        .test(window.navigator && navigator.userAgent),
+    imageMaxWidth: 800,
+    imageMaxHeight: 800,
+    imageCrop: true // Force cropped images
+})
+*/
