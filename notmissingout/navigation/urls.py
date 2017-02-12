5223efec670e192ea7d491c34632eea2d5acbe86
Richard Boulton <richard@tartarus.org>
2017-02-12 20:11:00 +0000

    Add generic navigation url

--- notmissingout/navigation/urls.py
+++ notmissingout/navigation/urls.py
@@ -5,7 +5,16 @@ from . import views
 app_name = 'navigation'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^little_learners/victorians/health$', views.victorian_health, name='victorian_health'),
-    url(r'^little_learners/victorians/communication$', views.victorian_communication, name='victorian_communication'),
+    url(
+        r'^little_learners/victorians/health$',
+        views.victorian_health,
+        name='victorian_health'
+    ),
+    url(
+        r'^little_learners/victorians/communication$',
+        views.victorian_communication,
+        name='victorian_communication'
+    ),
     url(r'^reflections$', views.reflections, name='reflections'),
+    url(r'^(?P<path>.*)/?$', views.navigation, name='navigation'),
 ]
--- notmissingout/navigation/views.py
+++ notmissingout/navigation/views.py
@@ -1,14 +1,53 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
+
 
 def index(request):
     return render(request, 'navigation/index.html')
 
+
 def victorian_health(request):
     return render(request, 'navigation/victorian_health.html')
+
 
 def victorian_communication(request):
     return render(request, 'navigation/victorian_communication.html')
 
+
 def reflections(request):
     return render(request, 'navigation/reflections.html')
+
+
+def navigation(request, path):
+    path = path.rstrip('/')
+
+    def lookup_section(path):
+        try:
+            slug = path.split('/')[-2]  # slug of the instance
+        except IndexError:
+            return None
+
+        candidates = Section.objects.filter(slug=slug)
+        for candidate in candidates:
+            if candidate.get_path() == path:
+                return candidate
+        return None
+
+    article_instance = None
+    section_instance = lookup_section(path)
+    if section_instance is None:
+        section_instance = lookup_section(path.rsplit('/', 1)[0])
+        if section_instance is not None:
+            article_instance = Article.objects.get(
+                section=section_instance,
+                slug = path.rsplit('/', 1)[1]
+            )
+            if article_instance is None:
+                raise Http404
+
+    if section_instance is None:
+        raise Http404
+
+    return render(
+        request, 'navigation/article.html', (path, instance, article)
+    )
